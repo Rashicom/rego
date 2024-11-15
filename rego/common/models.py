72523414a33fa06e43eeac9cf42609464e9896f8
Rashicom <rashid.kp484@gmail.com>
2024-11-15 22:07:25 +0530

    plan api Done

--- rego/common/models.py
+++ rego/common/models.py
@@ -5,7 +5,7 @@ class Plans(models.Model):
     class PeriodicityChoice(models.TextChoices):
         MONTH = 'Month',
         YEAR = 'Year'
-    plan_name = models.CharField(max_length=10)
+    plan_name = models.CharField(max_length=50)
     price = models.FloatField()
     periodicity = models.CharField(max_length=20, choices=PeriodicityChoice, default=PeriodicityChoice.MONTH)
 
@@ -13,11 +13,12 @@ class Plans(models.Model):
         return self.plan_name
     
 class PlanFeatures(models.Model):
-    plan = models.ForeignKey(Plans, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plans, on_delete=models.CASCADE, related_name="features")
     feature_description = models.TextField()
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.feature_description}-feature"
 
-
+    class Meta:
+        ordering = ['-is_active']
--- rego/common/views.py
+++ rego/common/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Plans
 
 
 class HomeView(View):
     def get(self, request):
-        return render(request, "index.html")
+        products = Plans.objects.all()
+        return render(request, "index.html", {"products":products})
 
 class BuisinessView(View):
     def get(self, request):
